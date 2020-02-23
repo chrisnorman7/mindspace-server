1f9ef8ca9edb7ea60cfc0a5521f9753b357251c5
Chris Norman <chris.norman2@googlemail.com>
2020-02-23 19:00:12 +0000

    Add shutdown command.

--- server/commands/admin.py
+++ server/commands/admin.py
@@ -1,8 +1,20 @@
-from ..parsers import main_parser, load_commands
+"""Provides administrator commands."""
+
+from twisted.internet import reactor
+
+from ..parsers import main_parser, load_commands, admin_required
 
 
 @main_parser.command
 def reload_commands(con):
     """Reload commands."""
+    admin_required(con.player)
     con.logger.info('Reloading commands...')
     load_commands()
+
+
+@main_parser.command
+def shutdown(con):
+    """Shutdown the server."""
+    admin_required(con.player)
+    reactor.stop()
