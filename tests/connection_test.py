4627266710d95ca5097ef87338636f98d2a8991b
Chris Norman <chris.norman2@googlemail.com>
2020-02-26 16:58:53 +0000

    Added a test for the message command.

--- tests/connection_test.py
+++ tests/connection_test.py
@@ -3,3 +3,10 @@ def test_alert(con):
     con.alert(msg)
     assert con.command_args == ('alert', msg)
     assert con.command_kwargs == {}
+
+
+def test_message(con):
+    msg = 'This is a test message.'
+    con.message(msg)
+    assert con.command_args == ('message', msg)
+    assert con.command_kwargs == {}
