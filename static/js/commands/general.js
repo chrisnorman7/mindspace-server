acbafad4cb69c54636e40b4ac7bc5504ef06a2d7
Chris Norman <chris.norman2@googlemail.com>
2020-02-26 17:11:54 +0000

    Added javascript confirm command.

--- static/js/commands/general.js
+++ static/js/commands/general.js
@@ -5,3 +5,19 @@ mindspace.addCommand("alert", alert)
 mindspace.addCommand("title", title => document.title = title)
 
 mindspace.addCommand("message", text => message.innerText = text)
+
+mindspace.addCommand(
+    "confirm", (
+        msg, okCommand, okArgs, okKwargs, cancelCommand, cancelArgs,cancelKwargs
+    ) => {
+        if (confirm(msg)) {
+            mindspace.sendCommand(
+                {name: okCommand, args: okArgs, kwargs: okKwargs}
+            )
+        } else {
+            mindspace.sendCommand(
+            {name: cancelCommand, args: cancelArgs, kwargs: cancelKwargs}
+            )
+        }
+    }
+)
